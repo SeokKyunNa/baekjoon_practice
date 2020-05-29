c07fc75c2dd4d1d394baa61e22c0b49c1f0f8e99
engsk <engsk1211@gmail.com>
2020-05-30 00:55:55 +0900

    part 1 - Question 2588

--- part001/src/part001/Q002588.java
+++ part001/src/part001/Q002588.java
@@ -9,7 +9,16 @@ public class Q002588 {
 		Scanner sc = new Scanner(System.in);
 		int num1 = sc.nextInt();
 		int num2 = sc.nextInt();
-		System.out.println();
+		int[] temp = new int[3];
+		
+		temp[0] = num2 / 100;	// 3
+		temp[1] = (num2 - (temp[0] * 100)) / 10;	// 8
+		temp[2] = num2 - ((temp[0] * 100) + (temp[1]) * 10); 
+		
+		System.out.println(num1 * temp[2]);
+		System.out.println(num1 * temp[1]);
+		System.out.println(num1 * temp[0]);
+		System.out.println(num1 * num2);
 	}
 
 }
