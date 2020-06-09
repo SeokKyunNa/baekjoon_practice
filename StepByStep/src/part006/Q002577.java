ba94a0fbfd4ac66080cc32e37b9850ae0aa77ce1
SeokKyunNa <engsk1211@gmail.com>
2020-06-09 17:22:08 +0900

    part 6 - array ( ~ 2 )

---
+++ StepByStep/src/part006/Q002577.java
@@ -0,0 +1,23 @@
+package part006;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q002577 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] num = new int[10];
+		int mul = 1;
+		
+		for(int i = 0; i < 3; i++) {
+			mul *= Integer.parseInt(br.readLine());
+		}
+		
+		// 한 자리 씩 잘라서 무슨 숫자인지 확인하고 num[n] 배열에 담기
+		for(int n:)
+	}
+
+}
