fa786f0e0248374d4d0d11c700fa3eb2ad74a2f9
engsk <engsk1211@gmail.com>
2020-06-11 23:04:56 +0900

    part 6 - array

--- StepByStep/src/part006/Q004344.java
+++ StepByStep/src/part006/Q004344.java
@@ -3,7 +3,7 @@ package part006;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.text.DecimalFormat;
 
 /* 평균은 넘겠지 */
 public class Q004344 {
@@ -11,20 +11,35 @@ public class Q004344 {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(br.readLine());
+		int sum = 0, average = 0, count = 0;
+		double ratio;
+		String ratio_s;
+		DecimalFormat df = new DecimalFormat("0.000");
 		
 		for(int i = 0; i < n; i ++) {
-			String[] score = br.readLine().split(" ");
-			for(int j = 0; j < Integer.parseInt(score[0]); j++) {
-				/*평균 구하고
-				 * 평균 넘으면 num++, 아니면 pass;
-				 * num/학생수 * 100; */
+			sum = 0; average = 0; count = 0; ratio = 0;
+			String[] score = br.readLine().split(" ");	
+			for(int j = 1; j <= Integer.parseInt(score[0]); j++) {
+				sum += Integer.parseInt(score[j]);
 			}
+			
+			average = sum / Integer.parseInt(score[0]);
+			
+			for(int j = 1; j <= Integer.parseInt(score[0]); j++) {
+				if(average < Integer.parseInt(score[j])) count++;
+			}
+			
+			ratio = count / (double)Integer.parseInt(score[0]) * 100;
+			ratio = Math.round(ratio * 1000) / 1000.0;
+			ratio_s = df.format(ratio);
+
+			sb.append(ratio_s + "%").append("\n");
+			
 		}
 		
-		
+		System.out.println(sb);
 		
 	}
 
