a648e2a3acb733fe3ff485eb525cc2a61f7df136
engsk <engsk1211@gmail.com>
2020-06-17 00:19:30 +0900

    part 8 - String ( ~ 5 )

--- StepByStep/src/part008_string/Q001157.java
+++ StepByStep/src/part008_string/Q001157.java
@@ -12,15 +12,27 @@ public class Q001157 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String word = br.readLine().toUpperCase();	// A = 65, a = 97
 		int[] alphabet = new int[26];
+		int max = 0, c = 0, overlap = 0;;
 
 		for(int i = 0; i < word.length(); i++) {
-			System.out.println(alphabet[word.charAt(i)-'A']);
 			alphabet[word.charAt(i)-'A'] += 1;
 		}
-		System.out.println();
-		for(int i : alphabet) {
-			System.out.println(alphabet[i]);
+		
+		for(int i = 0; i < alphabet.length; i++) {
+			if(max < alphabet[i]) {
+				max = alphabet[i];
+				c = i;
+			}
 		}
+		
+		for(int i = 0; i < alphabet.length; i++) {
+			if(max == alphabet[i]) overlap++;
+		}
+		
+		if(overlap > 1) c = -2;
+
+		System.out.println((char)(c + 'A'));
+		
 	}
 
 }
