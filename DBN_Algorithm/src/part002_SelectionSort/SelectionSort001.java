2f55d67b751c4fa2c64012e8eb060fd1a4dfbfa3
engsk <engsk1211@gmail.com>
2020-06-25 23:32:57 +0900

    DBN Algorithm part002 Selection Sort

---
+++ DBN_Algorithm/src/part002_SelectionSort/SelectionSort001.java
@@ -0,0 +1,39 @@
+package part002_SelectionSort;
+
+/* 선택 정렬 */
+/* 가장 작은 수를 맨 앞으로 보내는 알고리즘 */
+
+/* 시간 복잡도 */
+/* 10 + 9 + 8 + ... + 1 
+ * = 10 * (10 + 1) / 2 = 55 
+ * N * (N + 1) / 2 */
+/* N이 매우 큰 수라고 가정하고 +1과 /2는 생략함 */
+/* 시간 복잡도의 big O 표기법 (알고리즘의 연산 횟수를 표현) */
+/* O(N * N) = O(N^2) */
+public class SelectionSort001 {
+
+	public static void main(String[] args) {
+
+		int i, j, min, index = 0, temp;
+		int[] array = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+		
+		for(i = 0; i < 10; i++) {
+			min = Integer.MAX_VALUE;
+			for(j = i; j < 10; j++) {
+				if(min > array[j]) {
+					min = array[j];
+					index = j;
+				}
+			}
+			
+			temp = array[i];
+			array[i] = array[index];
+			array[index] = temp;
+		}
+		
+		for(i = 0; i < 10; i++) {
+			System.out.printf("%d ", array[i]);
+		}
+	}
+
+}
